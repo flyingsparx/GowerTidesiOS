13b42ca75c9624fe15fd3fe9caf63445ae399219
Will Webberley <willwebberley@gmail.com>
2016-08-21 19:46:44 +0100

    better styles for tide event table

--- GowerTidesiOS/tideEntryView.swift
+++ GowerTidesiOS/tideEntryView.swift
@@ -14,32 +14,32 @@ class TideEntryView: UIView {
     
     init(event: TideEvent, index: Int) {
         self.tideEvent = event
-        super.init(frame: CGRect(x: 85 * index, y: 15, width: 80, height: 45))
-        
+        super.init(frame: CGRect(x: 95 * index, y: 15, width: 90, height: 45))
+        layer.cornerRadius = 10.0
+        backgroundColor = UIColor.init(red: 0.89, green: 0.89, blue: 0.89, alpha: 1)
         let timeFormatter = NSDateFormatter()
         timeFormatter.dateFormat = "HH:mm"
         timeFormatter.locale = NSLocale(localeIdentifier: "en_GB_POSIX")
         
-        layer.cornerRadius = 10.0
-        backgroundColor = UIColor.whiteColor()
-        
-        let tideType = UILabel(frame: CGRect(x: 30, y: 6, width: 75, height: 17))
-        tideType.text = event.type.uppercaseString
-        tideType.textColor = UIColor.grayColor()
-        tideType.font = tideType.font.fontWithSize(14)
-        
         let icon = UIImageView(frame: CGRect(x: 5, y: 5, width: 20, height: 17))
         icon.image = UIImage(named: tideEvent.type)
         
-        let tideTime = UILabel(frame: CGRect(x: 5, y: 25, width:35, height:10))
+        let tideType = UILabel(frame: CGRect(x: 30, y: 6, width: 58, height: 17))
+        tideType.text = event.type.uppercaseString
+        tideType.textColor = UIColor.grayColor()
+        tideType.textAlignment = NSTextAlignment.Center
+        tideType.font = tideType.font.fontWithSize(14)
+        
+        let tideTime = UILabel(frame: CGRect(x: 5, y: 29, width:35, height:10))
         tideTime.text = timeFormatter.stringFromDate(event.time)
         tideTime.font = tideTime.font.fontWithSize(12)
         
-        let timeUntil = UILabel(frame:CGRect(x: 40, y: 25, width: 40, height: 10))
-        timeUntil.font = tideTime.font.fontWithSize(12)
+        let timeUntil = UILabel(frame:CGRect(x: 40, y: 29, width: 45, height: 10))
+        timeUntil.textAlignment = NSTextAlignment.Right
+        timeUntil.font = tideTime.font.fontWithSize(11)
         if event.day.isToday(){
             let now = NSDate()
-            timeUntil.textColor = UIColor.greenColor()
+            timeUntil.textColor = UIColor.init(red: 0.1, green: 0.5, blue: 0.1, alpha: 1)
             var labelSign = "+"
             var untilComponents = NSCalendar.currentCalendar().components([.Hour, .Minute], fromDate: now, toDate: event.time, options: [])
             
@@ -53,7 +53,7 @@ class TideEntryView: UIView {
             if mins < 10{
                 minString = "0" + String(mins)
             }
-            timeUntil.text = labelSign + String(untilComponents.hour) + ":" + minString
+            timeUntil.text = "(" + labelSign + String(untilComponents.hour) + ":" + minString + ")"
         }
         else{
             timeUntil.hidden = true
